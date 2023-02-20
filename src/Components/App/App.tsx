b383c072861bae5c4dd4d6baeeaf7647697c4a17
Robert Aiken <robertsiraaiken@gmail.com>
2023-02-20 17:47:11 -0600

    Add Station info and get request for info

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react"
 import "./App.scss"
-import fetchData from "../../fetchapis"
+import {fetchData} from "../../fetchapis"
 import Header from "../Header/Header.tsx"
 import {
   BrowserRouter as Router,
@@ -9,6 +9,7 @@ import {
 } from 'react-router-dom'
 import Form from "../Form/Form.tsx"
 import Splash from "../Splash/Splash.tsx"
+import Station from "../Station/Station.tsx"
 
 
 
@@ -35,6 +36,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Splash/>}/>
           <Route path="/form" element={<Form/>}/>
+          {/* <Route path="/:id" element={<Station/>}/> */}
         </Routes>
       </Router>
     </main>
