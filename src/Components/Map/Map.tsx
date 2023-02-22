0be353fd2b103cb171745a22085007d2615f0ead
Jocelle Bautista <jcllebautista@gmail.com>
2023-02-22 10:08:04 -0500

    Add Map component

---
+++ src/Components/Map/Map.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './Map.scss'
+import Station from '../Station/Station.tsx'
+
+export default function Map() {
+  return (
+    <div>
+      <Station />
+    </div>
+  )
+}
