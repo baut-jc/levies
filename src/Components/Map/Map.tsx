c68cfa12f000c01a08c9ce24cf80fa57f6b76450
Jocelle Bautista <jcllebautista@gmail.com>
2023-02-22 18:12:54 -0500

    Add station display after input

--- src/Components/Map/Map.tsx
+++ src/Components/Map/Map.tsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './Map.scss'
 import Station from '../Station/Station.tsx'
+import {fetchData} from "../../fetchapis"
+
 
 export default function Map() {
+  const [stations, setStations] = useState([])
+
+  useEffect(() => {
+    fetchData().then(data => {
+      console.log(data.length < 10)})
+  }, [])
+  // const showStations = (stationMarker) => {
+  //   setStations([...stations, stationMarker])
+  // }
+  // const stationsMap = stations.map(station => {
+  //   return (
+  //     <Station station=stat
+  //     />
+  //   )
+  // })
+
   return (
-    <div>
+    <div className='station-map'>
       <Station />
     </div>
   )
