50038e852f54b07db0c0deab7cfa2dccaa9dc4e5
Robert Aiken <robertsiraaiken@gmail.com>
2023-02-22 16:51:46 -0600

    Add splash page elements test

--- cypress/e2e/app_spec.cy.js
+++ cypress/e2e/app_spec.cy.js
@@ -1,6 +1,16 @@
 describe('Can run a test', () => {
+
+  beforeEach(()=>{
+    cy.visit('http://localhost:3000/')
+  })
+
   it('Should confirm that true is equal to true', () => {
     expect(true).to.equal(true)
   });
+
+  it('Should have a logo and a button on the splash page', ()=>{
+    cy.get('img[class="web-logo"]')
+    cy.get('button')
+  })
   }
 )
