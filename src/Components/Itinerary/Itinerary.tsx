21d516927991e8aa5edaa7dc87bcfd6fe2389c2d
Robert Aiken <robertsiraaiken@gmail.com>
2023-02-23 11:35:20 -0600

    Add itinerary skeleton

---
+++ src/Components/Itinerary/Itinerary.tsx
@@ -0,0 +1,14 @@
+import React, {useState} from "react"
+
+
+function Itinerary({itineraryStations}) {
+
+  return (
+    <div>
+      {itineraryStations}
+    </div>
+  )
+
+}
+
+export default Itinerary
