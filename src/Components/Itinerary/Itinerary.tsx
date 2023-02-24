74dd2c82d12a7cd7f45fd183f2c4abb7eabfeba3
Robert Aiken <robertsiraaiken@gmail.com>
2023-02-24 12:00:26 -0600

    Add message to empty itinerary page to direct user elsewhere

--- src/Components/Itinerary/Itinerary.tsx
+++ src/Components/Itinerary/Itinerary.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react"
 import Station from '../Station/Station.tsx'
-
+import Header from '../Header/Header.tsx'
 
 function Itinerary({itineraryStations, deleteItineraryStation}) {
   console.log(itineraryStations)
@@ -14,7 +14,8 @@ function Itinerary({itineraryStations, deleteItineraryStation}) {
 
   return (
     <div>
-      {displayItinerary}
+      <Header/>
+      {displayItinerary.length > 0 ? displayItinerary : <p>Nothing Here... Check out the Map to add Stations!</p>}
     </div>
   )
 
