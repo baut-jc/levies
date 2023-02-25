a017e6a1235ce14159d983871b89e221c87c8aff
Colin Ciervo <colinciervo@@gmail.com>
2023-02-25 13:13:22 -0500

    Add classname to form button

--- cypress/e2e/Itinerary.cy.ts
+++ cypress/e2e/Itinerary.cy.ts
@@ -6,9 +6,15 @@ describe('Itinterary page', () => {
         cy.get('p').contains('Nothing Here...')
     })
     it('should contain results', () => {
-        cy.intercept({
-            method: 'GET',
-            url: 'https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=ELEC&zip=undefined&limit=10&access=public&api_key=Z6T9IALxddG6bZYlBZ4IncLhusz3nVjdGSzv9Iu4'
-        }, { fixture: 'itinerary' })
+        // cy.intercept({
+        //     method: 'GET',
+        //     url: 'https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=ELEC&zip=undefined&limit=10&access=public&api_key=Z6T9IALxddG6bZYlBZ4IncLhusz3nVjdGSzv9Iu4'
+        // }, { fixture: 'itinerary' })
+        cy.visit('http://localhost:3000/form')
+        cy.get('[name="startPoint"]').type('06053')
+        cy.get('.submit').click()
+        cy.wait(1000)
+        cy.get(':nth-child(1) > .station-card > button')
+        cy.get('[href="/itinerary"]').click()
     })
 })
