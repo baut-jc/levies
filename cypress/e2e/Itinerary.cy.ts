578a96f739105b73c1bf5170d2f3b9a162c40129
Colin Ciervo <colinciervo@@gmail.com>
2023-02-25 13:30:32 -0500

    Add some tests

--- cypress/e2e/Itinerary.cy.ts
+++ cypress/e2e/Itinerary.cy.ts
@@ -6,10 +6,10 @@ describe('Itinterary page', () => {
         cy.get('p').contains('Nothing Here...')
     })
     it('should contain results', () => {
-        // cy.intercept({
-        //     method: 'GET',
-        //     url: 'https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=ELEC&zip=undefined&limit=10&access=public&api_key=Z6T9IALxddG6bZYlBZ4IncLhusz3nVjdGSzv9Iu4'
-        // }, { fixture: 'itinerary' })
+        cy.intercept({
+            method: 'GET',
+            url: 'https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=ELEC&zip=undefined&limit=10&access=public&api_key=Z6T9IALxddG6bZYlBZ4IncLhusz3nVjdGSzv9Iu4'
+        }, { fixture: 'itinerary' })
         cy.visit('http://localhost:3000/form')
         cy.get('[name="startPoint"]').type('06053')
         cy.get('.submit').click()
