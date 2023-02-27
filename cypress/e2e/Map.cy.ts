980ad4ce083a0154d95686f8884ba28f0fd6e734
Colin Ciervo <colinciervo@@gmail.com>
2023-02-27 17:45:22 -0500

    Add intercept for map

--- cypress/e2e/Map.cy.ts
+++ cypress/e2e/Map.cy.ts
@@ -1,17 +1,31 @@
 describe('Map page', () => {
-    beforeEach(() => {
-        cy.visit('http://localhost:3000/map')
-    })
-    it('should display the map', () => {
+    it('should display the map when Plan Trip button is clicked', () => {
+        cy.visit('http://localhost:3000/form')
+        cy.intercept({
+            method: 'GET',
+            url: 'https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=ELEC&zip=90011&limit=10&access=public&api_key=Z6T9IALxddG6bZYlBZ4IncLhusz3nVjdGSzv9Iu4',
+        }, { fixture: 'startPoint' })
+        cy.intercept({
+            method: 'GET',
+            url: 'https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=ELEC&zip=06053&limit=10&access=public&api_key=Z6T9IALxddG6bZYlBZ4IncLhusz3nVjdGSzv9Iu4',
+        }, { fixture: 'endPoint' })
+        cy.get('[name="startPoint"]').type('90011')   
+        cy.wait(500) 
+        cy.get('[name="endPoint"]').type('06053')
+        cy.get('[type="submit"]').click()
+        cy.get(':nth-child(1) > .station-card > :nth-child(1)').contains('Central Connecticut State University')
         
     })
 
-    it('should display an error component on a bad request', () => {
-        cy.intercept(`https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=ELEC&zip=&ev_connector_type=&limit=10&access=public&api_key=Z6T9IALxddG6bZYlBZ4IncLhusz3nVjdGSzv9Iu4`, {
-            statusCode: 404
+        it('should display an error component on a bad request', () => {
+            cy.intercept(`https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=ELEC&zip=06053&ev_connector_type=&limit=10&access=public&api_key=Z6T9IALxddG6bZYlBZ4IncLhusz3nVjdGSzv9Iu4`, {
+                statusCode: 404
+            })
+            cy.intercept(`https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=ELEC&zip=90011&ev_connector_type=&limit=10&access=public&api_key=Z6T9IALxddG6bZYlBZ4IncLhusz3nVjdGSzv9Iu4`, {
+                statusCode: 404
+            })
+            .get('p')
+            .contains('Something has Gone Wrong')
+            .get('button').click()
         })
-        .get('p')
-        .contains('Something has Gone Wrong')
-        .get('button').click()
-    })
 })
