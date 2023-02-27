8a864e2024e23719e31e9f383c671b51e4f0d0c5
Colin Ciervo <colinciervo@@gmail.com>
2023-02-27 17:51:50 -0500

    Add test for add stop button

--- cypress/e2e/Map.cy.ts
+++ cypress/e2e/Map.cy.ts
@@ -17,15 +17,35 @@ describe('Map page', () => {
         
     })
 
-        it('should display an error component on a bad request', () => {
-            cy.intercept(`https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=ELEC&zip=06053&ev_connector_type=&limit=10&access=public&api_key=Z6T9IALxddG6bZYlBZ4IncLhusz3nVjdGSzv9Iu4`, {
-                statusCode: 404
-            })
-            cy.intercept(`https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=ELEC&zip=90011&ev_connector_type=&limit=10&access=public&api_key=Z6T9IALxddG6bZYlBZ4IncLhusz3nVjdGSzv9Iu4`, {
-                statusCode: 404
-            })
-            .get('p')
-            .contains('Something has Gone Wrong')
-            .get('button').click()
-        })
+    it('should be able to add to itinerary', () => {
+        cy.visit('http://localhost:3000/form')
+        cy.intercept({
+            method: 'GET',
+            url: 'https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=ELEC&zip=90011&limit=10&access=public&api_key=Z6T9IALxddG6bZYlBZ4IncLhusz3nVjdGSzv9Iu4',
+        }, { fixture: 'startPoint' })
+        cy.intercept({
+            method: 'GET',
+            url: 'https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=ELEC&zip=06053&limit=10&access=public&api_key=Z6T9IALxddG6bZYlBZ4IncLhusz3nVjdGSzv9Iu4',
+        }, { fixture: 'endPoint' })
+        cy.get('[name="startPoint"]').type('90011')   
+        cy.wait(500) 
+        cy.get('[name="endPoint"]').type('06053')
+        cy.get('[type="submit"]').click()
+        cy.get(':nth-child(1) > .station-card > :nth-child(1)').contains('Central Connecticut State University')
+        cy.get(':nth-child(1) > .station-card > button').click()
+        cy.get(':nth-child(1) > .station-card > button').contains('Remove')
+        
+    })
+
+        // it('should display an error component on a bad request', () => {
+        //     cy.intercept(`https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=ELEC&zip=06053&ev_connector_type=&limit=10&access=public&api_key=Z6T9IALxddG6bZYlBZ4IncLhusz3nVjdGSzv9Iu4`, {
+        //         statusCode: 404
+        //     })
+        //     cy.intercept(`https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=ELEC&zip=90011&ev_connector_type=&limit=10&access=public&api_key=Z6T9IALxddG6bZYlBZ4IncLhusz3nVjdGSzv9Iu4`, {
+        //         statusCode: 404
+        //     })
+        //     .get('p')
+        //     .contains('Something has Gone Wrong')
+        //     .get('button').click()
+        // })
 })
